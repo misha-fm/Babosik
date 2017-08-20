e57b8504fa438920f90fd680ef5e2a94f78eb8a2
Карпов Михаил <misha_fm@mail.ru>
2017-08-20 21:26:57 +0300

    Added fat web project

---
+++ Babosik.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Babosik.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
